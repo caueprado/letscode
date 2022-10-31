bc48d5634b592dae7dc44835ea92af71f98c5fc5
Cauê do Prado <caueprado@gmail.com>
2022-10-31 18:30:24 -0300

    feat: propostas de resposta para analise

---
+++ src/main/java/br/ada/logic/proposta/Proposta3.java
@@ -0,0 +1,10 @@
+package br.ada.logic.proposta;
+
+public class Proposta3 {
+
+    public static void resposta(int a, int b) {
+        if(a >= b) {
+            System.out.println("a é maior que b");
+        }
+    }
+}
